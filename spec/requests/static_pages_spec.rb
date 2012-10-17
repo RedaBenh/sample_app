857de15930c832217be28fa2dfa8c8e8f08f75c0
redabenh <benh.reda@gmail.com>
2012-10-17 23:39:54 +0200

    change static page spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      page.should have_selector('h1', text:  'Sample App')
       
      end
 
